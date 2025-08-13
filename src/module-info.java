c59b5ab74105ec62fd769488035b18e5f9d0a47c
ZaloPerez <zaloperez+github@pm.me>
2025-08-13 21:17:28 +0200

    JPMS integration

---
+++ src/module-info.java
@@ -0,0 +1,7 @@
+module es.gonzagile.jmockload {
+    exports es.gonzagile.jmockload.core;
+    exports es.gonzagile.jmockload.constraints;
+    exports es.gonzagile.jmockload.commons;
+    exports es.gonzagile.jmockload.generators.dumb;
+    exports es.gonzagile.jmockload.generators.smart;
+}
